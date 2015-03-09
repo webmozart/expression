21ce91943d73bb70524a2d954b1d0b90f5d857f7
Bernhard Schussek <bschussek@gmail.com>
2015-03-09 11:39:44 +0100

    Implemented NotSame

---
+++ tests/Atom/NotSameTest.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the webmozart/criteria package.
+ *
+ * (c) Bernhard Schussek <bschussek@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Webmozart\Criteria\Tests\Atom;
+
+use PHPUnit_Framework_TestCase;
+use Webmozart\Criteria\Atom\NotSame;
+
+/**
+ * @since  1.0
+ * @author Bernhard Schussek <bschussek@gmail.com>
+ */
+class NotSameTest extends PHPUnit_Framework_TestCase
+{
+    public function testMatch()
+    {
+        $criterion = new NotSame('amount', '10');
+
+        $this->assertTrue($criterion->match(array('amount' => '100')));
+        $this->assertTrue($criterion->match(array('amount' => 11)));
+        $this->assertTrue($criterion->match(array('amount' => 10)));
+        $this->assertTrue($criterion->match(array('amount' => 10.0)));
+        $this->assertFalse($criterion->match(array('amount' => '10')));
+    }
+}
