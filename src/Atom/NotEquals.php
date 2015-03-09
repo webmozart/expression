6fdfb31c8d5ddede3a889d3b94e885b9576b1b9a
Bernhard Schussek <bschussek@gmail.com>
2015-03-09 11:37:16 +0100

    Implemented NotEquals

---
+++ src/Atom/NotEquals.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the webmozart/criteria package.
+ *
+ * (c) Bernhard Schussek <bschussek@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Webmozart\Criteria\Atom;
+
+/**
+ * Checks that a field value does not equal another value.
+ *
+ * The comparison is done using PHP's "!=" equality operator.
+ *
+ * @since  1.0
+ * @author Bernhard Schussek <bschussek@gmail.com>
+ */
+class NotEquals extends Atom
+{
+    /**
+     * @var mixed
+     */
+    private $value;
+
+    /**
+     * Creates the criterion.
+     *
+     * @param string $field The field name.
+     * @param mixed  $value The compared value.
+     */
+    public function __construct($field, $value)
+    {
+        parent::__construct($field);
+
+        $this->value = $value;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function matchValue($value)
+    {
+        return $this->value != $value;
+    }
+}
