a791619f77a06d3867d1ff20a061881df73903a9
Bernhard Schussek <bschussek@gmail.com>
2015-03-10 12:26:23 +0100

    Fixed file headers

--- tests/Comparison/EqualsTest.php
+++ tests/Comparison/EqualsTest.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the webmozart/criteria package.
+ * This file is part of the webmozart/expression package.
  *
  * (c) Bernhard Schussek <bschussek@gmail.com>
  *
